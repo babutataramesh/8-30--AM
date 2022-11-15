11f86ff494eceb96a68727318236c2346629fa51
root <root@localhost>
2022-11-15 03:41:18 +0000

    java code

---
+++ demo.java
@@ -0,0 +1,10 @@
+class Demo
+{
+  public static void main(...)
+  {
+    s.o.print(" Hello world ");
+    s.o.print(" Hello world ");
+    s.o.print(" Hello world ");
+    s.o.print(" Hello world ");
+  }
+}
