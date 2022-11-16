ca38371d9ed81aa66d6487849268f6d416766ae8
root <root@ip-172-31-89-139.ec2.internal>
2022-11-16 10:03:38 +0000

    this new commit

--- demo.java
+++ demo.java
@@ -4,4 +4,21 @@ class Demo
   {
     s.o.print(" Hello world ");
   }
-} 
+}
+
+	Hello
+
+
+
+class Demo
+{
+  public static void main(...)
+  {
+class Demo
+class Demo
+{
+  public static void main(...)
+  {
+{
+  public static void main(...)
+  {
