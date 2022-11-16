5f025c2bd24d56da8d74700c478b9182b717c5b5
root <root@localhost>
2022-11-16 03:35:24 +0000

    test code

---
+++ test.java
@@ -0,0 +1,11 @@
+class test
+{
+  public static void main(....)
+  {
+    s.o.print(" Hello !! ")
+    s.o.print(" Hello !! ")
+    s.o.print(" Hello !! ")
+    s.o.print(" Hello !! ")
+    s.o.print(" Hello !! ")
+  }
+}
